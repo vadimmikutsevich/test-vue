3bf18b601e7d89ad3d7b75de59471f546856fcae
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2024-09-27 17:58:53 +0400

    implement blog page

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Post } from "../stores/postsStore";
 
-<template></template>
+interface Props {
+  item: Post;
+}
+
+const { item } = defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <h5 class="font-medium text-2xl desktop:text-3xl">{{ item.title }}</h5>
+
+    <div class="mt-1.5 desktop:mt-4 desktop:text-lg">
+      <span class="mr-6">{{ item.date }}</span>
+      <span class="mr-6">|</span>
+      <span v-for="(category, index) in item.categories" :key="index">
+        {{ category }}<span v-if="index < item.categories.length - 1">, </span>
+      </span>
+    </div>
+
+    <p class="mt-4 desktop:mt-1.5">{{ item.content }}</p>
+
+    <div class="bg-light h-[1px] mt-4 opacity-50" />
+  </div>
+</template>
 
 <style scoped></style>
