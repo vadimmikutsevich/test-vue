1b75404556a1819bc7143bf4777694a3a155e199
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2024-09-27 12:26:08 +0400

    add main layout with header and footer

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
-<template><h2>Blog</h2></template>
+<template>
+  <h2>Blog</h2>
+</template>
 
 <style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#21243D",
+        primary: "#FF6464",
+        secondary: "#00A8CC",
+        light: "#8695A4",
+      },
+    },
     screens: {
       mobile: { max: "640px" },
       tablet: { min: "641px", max: "1024px" },
